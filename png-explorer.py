bd28b5cfb74b2d706d9b47582984fbff4e5432c7
elmahdigaga <gaga.elmahdi@gmail.com>
2025-01-12 16:17:47 +0100

    (ft): Add ierator method to traverse hex data using the number of bytes

--- png-explorer.py
+++ png-explorer.py
@@ -3,4 +3,8 @@ image_path = "./dice.png"
 with open(image_path, "rb") as f:
     hexData = f.read().hex()
 
-print(hexData)
+def iterator(current, start, stop, nb_bytes, data):
+    start = current
+    stop = current + (nb_bytes * 2)
+    current = stop
+    return data[start:stop]
