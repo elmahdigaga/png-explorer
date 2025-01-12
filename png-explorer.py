871c562d712a73add4b2e28f959107b96940b43d
elmahdigaga <gaga.elmahdi@gmail.com>
2025-01-12 17:33:28 +0100

    (ft): Read the data length of a chunk

--- png-explorer.py
+++ png-explorer.py
@@ -17,11 +17,21 @@ def iterator(current, nb_bytes, data):
     start = current
     stop = current + (nb_bytes * 2)
     current = stop
-    return data[start:stop]
+    return data[start:stop], current
 
+
+current = 0
 # Get the signature (size: 8 bytes)
-signature_hex = iterator(0, 8, hex_data)
+signature_hex, current = iterator(current, 8, hex_data)
 if not signature_hex == "89504e470d0a1a0a":
     print("Error: Invalid signature")
     exit(1)
 print("Signature : ", signature_hex)
+
+
+### CHUNK: 4 bytes: length | 4 bytes: Type | x bytes: Data | 4 bytes: CRC
+
+# Get the chunk data length (size: 4 bytes)
+data_length_hex, current = iterator(current, 4, hex_data)
+data_length = int(data_length_hex, 16)
+print("Chunk data length : ", data_length)
